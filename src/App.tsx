c6fe04604196b010af7592e9718df01d351aa283
Hernan Mirx <hernanmirx@gmail.com>
2023-01-25 10:55:25 -0300

    primer boiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Index from './pages/Index';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
